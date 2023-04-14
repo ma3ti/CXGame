e96200fe30c962f9cddd53964d8b06012975ec46
ma3ti <matteo.fontana9@libero.it>
2023-04-14 12:04:23 +0200

    add README file and initialize Spycesar player as L1 player

---
+++ README.md
@@ -0,0 +1,20 @@
+## STRATEGY
+
+
+## STARTING THE GAME AS FIRST
+
+Put your checker in the center column if you're first.
+
+Because a checker in the center column allows you to make a 
+Connect 4 in all possible directions, this is the best possible first move.
+
+
+## STARTING THE GAME AS SECOND  
+
+Play your checker on top of your opponent's if you're second.
+
+By putting your checker on top of your opponent's checker in the same column, 
+you can guarantee at least a draw.
+
+This strategy works best if your opponent drops their first checker in the center column.
+If they put it somewhere else, take the center column to gain the upper hand.
--- connectx/SpycesarPlayer/Spycesar.java
+++ connectx/SpycesarPlayer/Spycesar.java
@@ -5,6 +5,8 @@ import connectx.CXGameState;
 import connectx.CXPlayer;
 
 import java.util.Random;
+import java.util.TreeSet;
+import java.util.concurrent.TimeoutException;
 
 public class Spycesar implements CXPlayer {
 
@@ -30,8 +32,86 @@ public class Spycesar implements CXPlayer {
 
     @Override
     public int selectColumn(CXBoard B) {
-        return 0;
+
+        START = System.currentTimeMillis(); // Save starting time
+
+        Integer[] L = B.getAvailableColumns();
+        int save    = L[rand.nextInt(L.length)]; // Save a random column
+
+        try {
+            int col = singleMoveWin(B,L);
+            if(col != -1)
+                return col;
+            else
+                return singleMoveBlock(B,L);
+        } catch (TimeoutException e) {
+            System.err.println("Timeout!!! Random column selected");
+            return save;
+        }
     }
+
+    private void checktime() throws TimeoutException {
+        if ((System.currentTimeMillis() - START) / 1000.0 >= TIMEOUT * (99.0 / 100.0))
+            throw new TimeoutException();
+    }
+
+    /**
+     * Check if we can win in a single move
+     *
+     * Returns the winning column if there is one, otherwise -1
+     */
+    private int singleMoveWin(CXBoard B, Integer[] L) throws TimeoutException {
+        for(int i : L) {
+            checktime(); // Check timeout at every iteration
+            CXGameState state = B.markColumn(i);
+            if (state == myWin)
+                return i; // Winning column found: return immediately
+            B.unmarkColumn();
+        }
+        return -1;
+    }
+
+
+
+    /**
+     * Check if we can block adversary's victory
+     *
+     * Returns a blocking column if there is one, otherwise a random one
+     */
+    private int singleMoveBlock(CXBoard B, Integer[] L) throws TimeoutException {
+        TreeSet<Integer> T = new TreeSet<Integer>(); // We collect here safe column indexes
+
+        for(int i : L) {
+            checktime();
+            T.add(i); // We consider column i as a possible move
+            B.markColumn(i);
+
+            int j;
+            boolean stop;
+
+            for(j = 0, stop=false; j < L.length && !stop; j++) {
+                //try {Thread.sleep((int)(0.2*1000*TIMEOUT));} catch (Exception e) {} // Uncomment to test timeout
+                checktime();
+                if(!B.fullColumn(L[j])) {
+                    CXGameState state = B.markColumn(L[j]);
+                    if (state == yourWin) {
+                        T.remove(i); // We ignore the i-th column as a possible move
+                        stop = true; // We don't need to check more
+                    }
+                    B.unmarkColumn(); //
+                }
+            }
+            B.unmarkColumn();
+        }
+
+        if (T.size() > 0) {
+            Integer[] X = T.toArray(new Integer[T.size()]);
+            return X[rand.nextInt(X.length)];
+        } else {
+            return L[rand.nextInt(L.length)];
+        }
+    }
+
 
     @Override
     public String playerName() {
